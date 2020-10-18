d2daed732e5f5427d2d0283dfb29b9f6a1baa859
Syifa <nurasysyifaufissudur@student.telkomuniversity.ac.id>
2020-10-18 12:33:57 +0700

    starting menu

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public int numPlayers = 2;
+
+    public static DataManager Instance { get; private set; } // static singleton
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+        // Cache references to all desired variables
+        //  player= FindObjectOfType<Player>();
+    }
+
+}
