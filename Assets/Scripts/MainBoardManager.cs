1281b5e9eaed8a96f2e9699ba252d34f96640e08
Syifa <nurasysyifaufissudur@student.telkomuniversity.ac.id>
2020-11-03 07:03:44 +0700

    main turn and dice

--- Assets/Scripts/MainBoardManager.cs
+++ Assets/Scripts/MainBoardManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainBoardManager : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class MainBoardManager : MonoBehaviour
 
     public Transform[] playerPositions;
 
+    public int curIndexPlayer;
+
+    public string state = "idle";
+
+//UI
+    public Text diceText;
+    public Text playerText;
 
     void Awake()
     {
@@ -15,6 +23,23 @@ public class MainBoardManager : MonoBehaviour
         {
             Instantiate(players[i], playerPositions[i].position, Quaternion.identity);
         }
+        playerText.text = DataManager.Instance.playerNames[0];
+    }
+
+    public void RollDice()
+    {
+        diceText.text = Random.Range(1, 6).ToString();
+        nextPlayerIndex();
+        playerText.text = DataManager.Instance.playerNames[curIndexPlayer];
+
+    }
+
+    public void nextPlayerIndex()
+    {
+        if (curIndexPlayer == DataManager.Instance.numPlayers - 1)
+            curIndexPlayer = 0;
+        else
+            curIndexPlayer++;
     }
 
 }
